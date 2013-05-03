c579dc7081a9be0fd4f04c786a40cd2d47cb5cb5
Poren Chiang <ren.chiang@gmail.com>
2013-05-04 02:22:32 +0800

    sharplex implementation

---
+++ test/sharp/SharpLex/ConsoleHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Allegro.Test.SharpLex
+{
+    internal class ConsoleHelper
+    {
+        public ConsoleHelper Text(string value)
+        {
+            Console.Write(value);
+            return this;
+        }
+
+        public ConsoleHelper Text(ConsoleColor color, string value)
+        {
+            ConsoleColor original = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.Write(value);
+            Console.ForegroundColor = original;
+            return this;
+        }
+
+        public ConsoleHelper Line(string value)
+        {
+            Console.WriteLine(value);
+            return this;
+        }
+
+        public ConsoleHelper Line(ConsoleColor color, string value)
+        {
+            ConsoleColor original = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(value);
+            Console.ForegroundColor = original;
+            return this;
+        }
+
+        public ConsoleHelper Error(string value)
+        {
+            ConsoleColor original = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Error.WriteLine(value);
+            Console.ForegroundColor = original;
+            return this;
+        }
+    }
+}
---
+++ test/sharp/SharpLex/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+
+namespace Allegro.Test.SharpLex
+{
+    internal class Program
+    {
+        static ConsoleHelper buffer = new ConsoleHelper();
+
+        static void Main(string[] args)
+        {
+            TextReader reader = Console.In;
+            LexicalProcessor lexer = null;
+            try
+            {
+                if (args.Length > 0) {
+                    if (args[0].ToLowerInvariant().Equals("-h"))
+                        PrintUsage();
+                    reader = new StreamReader(args[0]);
+                }
+
+                lexer = new LexicalProcessor(reader);
+                while (lexer.Read())
+                {
+                    LexicalToken token = lexer.Current;
+                    buffer.Text(ConsoleColor.DarkYellow, String.Format("[{0}]", GetEnumName(token.Type)))
+                          .Text(" ")
+                          .Line(String.Format(@"""{0}"" ""{1}""", token.Value, token.Tag));
+                }
+            }
+            catch (Exception ex) {
+                Exception e = ex;
+                while (e != null) {
+                    buffer.Error(String.Format("sharplex: {0} ({1})", e.Message, e.GetType().Name));
+                    e = ex.InnerException;
+                }
+
+                if (lexer != null)
+                    buffer.Error(String.Format("\t at line {0}, column {1}", lexer.Line, lexer.Column));
+
+                Environment.Exit(Math.Min(-ex.GetHashCode(), -1));
+            }
+        }
+
+        static void PrintUsage()
+        {
+            Console.WriteLine(
+                @"Usage: sharplex [file] | -h"
+            );
+            Environment.Exit(0);
+        }
+
+        static string GetEnumName<T>(T value)
+        {
+            return Enum.GetName(typeof(T), value);
+        }
+    }
+}
