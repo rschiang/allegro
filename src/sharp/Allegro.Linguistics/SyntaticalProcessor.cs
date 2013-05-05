4bfdc39432924a6d20a72b3fb4deb2960a0ad405
Poren Chiang <ren.chiang@gmail.com>
2013-05-05 23:26:46 +0800

    Start implementing SyntaticalProcessor

--- src/sharp/Allegro.Linguistics/SyntaticalProcessor.cs
+++ src/sharp/Allegro.Linguistics/SyntaticalProcessor.cs
@@ -4,7 +4,25 @@ using System.Text;
 
 namespace Allegro
 {
-    class SyntaticalProcessor
+    /// <summary>
+    /// Provides functionality to translate lexical tokens from Allegro source.
+    /// </summary>
+    public class SyntaticalProcessor
     {
+        #region "Constructor"
+        /// <summary>
+        /// Creates a new instance of <c>LexicalProcessor</c>.
+        /// </summary>
+        /// <param name="source">An enumerable sequence of lexical tokens.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <c>source</c> is <c>null</c>.</exception>
+    	public SyntaticalProcessor(IEnumerable<LexicalToken> source)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+        }
+        #endregion
+
+        #region "Methods"
+        
+        #endregion
     }
 }
