17f0aa9e859e63ef5d1dae1885ce248b7cb5a589
Poren Chiang <ren.chiang@gmail.com>
2013-05-07 01:53:00 +0800

    Start implementing SyntaticalProcessor public interface

--- src/sharp/Allegro.Linguistics/SyntaticalProcessor.cs
+++ src/sharp/Allegro.Linguistics/SyntaticalProcessor.cs
@@ -18,11 +18,29 @@ namespace Allegro
     	public SyntaticalProcessor(IEnumerable<LexicalToken> source)
         {
             if (source == null) throw new ArgumentNullException("source");
+            sourceBuffer = source;
         }
         #endregion
 
         #region "Methods"
+
+        /// <summary>
+        /// Parse the next syntatical element.
+        /// </summary>
+        /// <returns><c>true</c> if the next token was read successfully; otherwise, <c>false</c>.</returns>
+        public virtual bool Read()
+        {
+            // TODO
+        }
+        #endregion
+
+        #region "Properties"
         
+        #endregion
+
+        #region "Fields"
+        protected IEnumerable<LexicalToken> sourceBuffer;
+        private IList<LexicalToken> queue;
         #endregion
     }
 }
